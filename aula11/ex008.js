b0dbd5aaddbff949ae64b9469af7df7d356bb978
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2025-03-19 22:51:24 -0300

    Condições (Parte 1)

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if(vel > 60){ //Condição Simples
+    console.log(`Voce ultrapassou a velocidade permitita, Multado`)
+}
+console.log(`Se beber não dirija`)
